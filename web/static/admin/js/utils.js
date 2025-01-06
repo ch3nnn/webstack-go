c9d7b039e4485e5cdc7e39e87fa3a4c5a54f69da
ch3nnn <chenyinren@vip.qq.com>
2025-01-06 17:29:47 +0800

    refactor(site): Reconstructing the Parameters selectedIds the Category Selector Function

--- web/static/admin/js/utils.js
+++ web/static/admin/js/utils.js
@@ -12,3 +12,37 @@ function buildTree(list, pid = 0) {
             children: buildTree(list, item.id) // 递归查找子节点
         }));
 }
+
+
+/**
+ * 递归渲染树形结构到 <select>
+ * @param {Array} data - 树形结构数据
+ * @param {HTMLElement} parent - 父元素
+ * @param {number} level - 当前层级（用于缩进）
+ * @param {Array} selectedIds - 需要选中的选项 ID 列表
+ */
+function renderTreeToSelect(data, parent, level = 0, selectedIds = []) {
+    data.forEach(item => {
+        if (item.children && item.children.length > 0) {
+            // 如果是分组，创建 <optgroup>
+            const optgroup = document.createElement('optgroup');
+            optgroup.label = item.name;
+            parent.appendChild(optgroup);
+
+            // 递归渲染子节点
+            renderTreeToSelect(item.children, optgroup, level + 1, selectedIds);
+        } else {
+            // 如果是选项，创建 <option>
+            const option = document.createElement('option');
+            option.value = item.id;
+            option.text = ' '.repeat(level * 4) + item.name; // 缩进显示
+
+            // 如果当前选项 ID 在 selectedIds 中，则设置为选中
+            if (selectedIds.includes(item.id)) {
+                option.selected = true;
+            }
+
+            parent.appendChild(option);
+        }
+    });
+}
